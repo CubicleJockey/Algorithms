722b2b14b80eb0922809cacc55b1b81fcfa7cb65
André Davis <CubicleJockey@users.noreply.github.com>
2017-01-18 15:36:46 -0800

    Skeleton Setup
    A place to practice some algorithms for sorting.

---
+++ Sorting/SortingAlgorithms/Sorting.Tests/NumberSortingTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sorting.Tests
+{
+    [TestClass]
+    public class NumberSortingTests
+    {
+
+    }
+}
---
+++ Sorting/SortingAlgorithms/Sorting/ReadMe.md
@@ -0,0 +1,3 @@
+﻿#### Sorting Algorithm Library
+<hr>
+
