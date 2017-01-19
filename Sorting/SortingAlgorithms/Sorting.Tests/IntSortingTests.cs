e305c1cb9e0574404ecc6ef4910509e10c28b64a
André Davis <CubicleJockey@users.noreply.github.com>
2017-01-18 16:31:40 -0800

    Starting some basic bubble sort

---
+++ Sorting/SortingAlgorithms/Sorting.Tests/IntSortingTests.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sorting.Tests
+{
+    [TestClass]
+    public class NumberSortingTests
+    {
+        private readonly IntSorting sorter;
+
+        public NumberSortingTests()
+        {
+            sorter = new IntSorting();
+        }
+
+        [TestMethod]
+        public void BubbleSort_IntSmall()
+        {
+            IEnumerable<int> unsortedNumbers = new[] {5, 10, 32, 1, 16, 34, 56};
+            
+            var result = sorter.BubbleSort(unsortedNumbers).ToArray();
+
+            WriteLine($"Result: {string.Join(", ", result)}");
+
+            //1, 5, 10, 16, 32, 34, 56
+            result.Should().BeInAscendingOrder();
+        }
+    }
+}
---
+++ Sorting/SortingAlgorithms/Sorting/BaseNumberSorting.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sorting
+{
+    public abstract class BaseNumberSorting<TNumber> where TNumber : IComparable<TNumber>
+    {
+        /// <summary>
+        /// Bubble Sort (aka - Sinking Sort)
+        /// <br/>
+        /// <remarks>
+        /// Performance: <br/>
+        /// *Worst-Case: O(n^2) <br/>
+        /// *Best-Case:  O(n^2) <br/>
+        /// *Average:    O(n^2) <br/>
+        /// </remarks>
+        /// </summary>
+        /// <returns>A collection of numbers sorted in Ascending Order.</returns>
+        public virtual IEnumerable<TNumber> BubbleSort(IEnumerable<TNumber> unsortedNumbers)
+        {
+            IList<TNumber> numbers = unsortedNumbers.ToArray();
+
+            //Make sure to run through each number 
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                //Pass number through entire array
+                for(var current = 0; current < (numbers.Count - 1); current++)
+                {
+                    var next = (current + 1);
+                    if(numbers[current].CompareTo(numbers[next]) > 0)
+                    {
+                        Swap(numbers, current, next);
+                    }
+                }
+            }
+            return numbers;
+        }
+
+
+        #region Helper Methods
+
+        private static void Swap(IList<TNumber> numbers, int current, int next)
+        {
+            var temp = numbers[next];
+            numbers[next] = numbers[current];
+            numbers[current] = temp;
+        }
+
+        #endregion  Helper Methods
+
+    }
+}
